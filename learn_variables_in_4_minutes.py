0a7267fff2b8eafa8c3ec559b1ae196143217eef
llusty1 <103339838+llusty1@users.noreply.github.com>
2025-09-17 23:05:02 -0500

    Commit

---
+++ learn_variables_in_4_minutes.py
@@ -0,0 +1,13 @@
+# Variable names cannot start with numbers
+# Only letters, numbers, and underscores (_) can be used
+# Spaces are not allowed in variable names
+# Varialbe names are case sensitive
+
+my_name = "Summer"
+print(my_name.upper())
+
+my_age = 22
+print(my_age)
+
+fav_3_desserts = ["Cake", "Pie", "Fudge"]
+print(fav_3_desserts)
