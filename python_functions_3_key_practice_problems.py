89258139a673c1cb5f53cfd4e7ec0c86a4a5930d
llusty1 <103339838+llusty1@users.noreply.github.com>
2025-09-17 22:10:18 -0500

    Commit

---
+++ python_functions_3_key_practice_problems.py
@@ -0,0 +1,14 @@
+# Problem 1
+# It's important to assign return values to variables if you'd like to use them later in your program
+# What will be the output of the following code?
+
+
+def greet(name):
+    return "Hello " + name
+
+greeting = greet("Alice")
+greeting = greet("Bob")
+print(greeting)
+
+# Problem 2
+# 
