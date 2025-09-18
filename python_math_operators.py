e4773a69bfbd3a8e63bd0cc40b2c9b7760336a07
llusty1 <103339838+llusty1@users.noreply.github.com>
2025-09-17 22:51:53 -0500

    Commit

--- python_math_operators.py
+++ python_math_operators.py
@@ -5,3 +5,10 @@ print(2 + 3)
 print(2 - 3)
 print(2 * 3)
 print(2 / 3)
+
+# These are more advanced operators ** // %
+# Operators ** Exponent // Floor Division % Modulus
+
+print(10 ** 3)
+print(13 // 3)
+print(13 % 3)
