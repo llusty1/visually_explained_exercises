37ea427739aabb5354a27cbe548789edd230bb11
llusty1 <103339838+llusty1@users.noreply.github.com>
2025-09-17 22:38:45 -0500

    Commit

--- python_functions_3_key_practice_problems.py
+++ python_functions_3_key_practice_problems.py
@@ -11,4 +11,25 @@ greeting = greet("Bob")
 print(greeting)
 
 # Problem 2
-# 
+# The return value of on function call can be used as an argument to another function call.
+# What will this code output?
+
+def add(a, b):
+    return a + b
+
+result = add(5, 10)
+print(result)
+print(add (result, 20))
+
+# Problem 3 
+# Python allows us to use multiple return statements inside a function.
+# What will this code output?
+
+def compute(a, b):
+    result = a * b
+    if result > 10:
+        return result - 3
+    return result + 4
+    
+print(compute(3, 2))
+print(compute(5, 3))
